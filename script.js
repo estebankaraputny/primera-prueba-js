908fdd97ff1eec2928b855e8fc0add7d42d356a3
estebankaraputny <estebankaraputny18@gmail.com>
2022-10-13 18:08:59 -0300

    :rocket: New: my example of javascript

---
+++ script.js
@@ -0,0 +1,33 @@
+var name = "Esteban";
+var subname = "Karaputny";
+var pareja = False;
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var colegas = ["Cesar", "Ivan", "Jose", "Camila", "Leonardo"];
+
+var info={
+    name: "Esteban",
+    subname: "Karaputny",
+    pareja: False,
+    numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    colegas: ["Cesar", "Ivan", "Jose", "Camila", "Leonardo"],
+}
+
+
+var infoUser = {
+    nombre: "Esteban",
+    apellido: "Karaputny",
+    edad: 22,
+    email: "estebankaraputny18@gmail.com",
+    numerosFav:[18, 22, 10],
+
+    petsOne:{
+        nombrePet: "Lia",
+        edadPet: 2,
+    },
+
+    petsTwo :{
+        nombrePetTwo:"Oreja",
+        edadPetTwo: 3,
+    }
+
+}
